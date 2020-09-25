8c084ba8623c9c8323fdc48fe0046ebd8694b0cd
HironobuTozuka <69956837+HironobuTozuka@users.noreply.github.com>
2020-09-25 17:49:04 +0900

    Update CraneDevice.cs

--- RcsLogic/Crane/CraneDevice.cs
+++ RcsLogic/Crane/CraneDevice.cs
@@ -161,7 +161,6 @@ namespace RcsLogic.Crane
 
         public void HandleTaskBundleRemoved()
         {
-llll
             lock (this)
             {
                 _logger.LogInformation("deviceId: {1}, task bundle was removed, planning moves if necessary",
